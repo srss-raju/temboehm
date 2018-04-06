6cc3061ca0b1a842a68254e107592692dd6b0b8a
Murali Iyengar <miyengar@Nagaraj-MacBook-Pro.local>
2018-04-06 20:58:12 +0530

    Fixed issues and improved the code

--- flask_socket_server/sentiment.py
+++ flask_socket_server/sentiment.py
@@ -1,19 +1,16 @@
 from nltk.sentiment.vader import SentimentIntensityAnalyzer
-sid = SentimentIntensityAnalyzer()
-scores = sid.polarity_scores(message_text)
-testString = None
-def getScore(message_text):
-    print(message_text)
-    scores = sid.polarity_scores(message_text)
-    
-    for key in sorted(scores):
-        if scores['neu'] > scores['pos'] and scores['neu'] > scores['neg']:
-            print("Neutral")
-        elif scores['pos'] > scores['neu'] and scores['pos'] > scores['neg']:
-            print("Positive")
-        else:
-            print("Negative")   
-        break
 
-    return testString
-    #print('{0}: {1}, '.format(key, scores[key]), end='')
+sid = SentimentIntensityAnalyzer()
+
+def getPolarity(message_text):
+    polarity_scores = sid.polarity_scores(message_text)
+    print polarity_scores
+    
+    # return polarity_scores.['compound']
+
+    polarity_scores.pop('compound')
+    
+    polarities = polarity_scores.keys()
+    scores     = polarity_scores.values()
+
+    return polarities[scores.index(max(scores))]
